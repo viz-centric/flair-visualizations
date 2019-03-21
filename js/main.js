1623cc2f5bcb431fd3fd7b0993d649685bb4eab6
Suvrat Ram Joshi <suvrat.joshi@logpoint.com>
2019-03-21 18:15:11 +0545

    Commit #1: Initial implementation of charts 1. Charts included:     a. Clustered Vertical Bar     b. Infographics     c. KPI     d. Line     e. Pie 2. Implemented compass project for automatic transpilation of SASS to CSS 3. Organized few files and folder structure for better management 4. Updated bower.json file to include the dependencies and other important informations

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var clusteredverticalbar = require('./charts/clusteredverticalbar.js');
+var infographics = require('./charts/infographics.js');
+var kpi = require('./charts/kpi.js');
+var line = require('./charts/line.js');
+var pie = require('./charts/pie.js');
+
+
+module.exports = {
+	clusteredverticalbar: clusteredverticalbar,
+	infographics: infographics,
+	kpi: kpi,
+	line: line,
+	pie: pie
+}
